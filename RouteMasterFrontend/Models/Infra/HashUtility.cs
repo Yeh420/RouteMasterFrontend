bcf6b358ef5943d2ccffa4895aafbae46955e07e
Yeh420 <blizzard255324@gmail.com>
2023-07-30 18:01:03 +0800

    加入專案檔案。

---
+++ RouteMasterFrontend/Models/Infra/HashUtility.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RouteMasterFrontend.Models.Infra
+{
+	//直接貼，版本有差異可能需要修正
+	public class HashUtility
+	{
+		public static string ToSHA256(string plainText, string salt)
+		{
+			// ref https://docs.microsoft.com/zh-tw/dotnet/api/system.security.cryptography.sha256?view=net-6.0
+			using (var mySHA256 = SHA256.Create())
+			{
+				var passwordBytes = Encoding.UTF8.GetBytes(salt + plainText);
+				var hash = mySHA256.ComputeHash(passwordBytes);
+				var sb = new StringBuilder();
+				foreach (var b in hash) sb.Append(b.ToString("X2"));
+
+				return sb.ToString();
+			}
+		}
+
+		public static string GetSalt()
+		{
+			return "";
+			//return System.Configuration.ConfigurationManager.AppSettings["Salt"];
+		}
+	}
+}
---
+++ RouteMasterFrontend/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RouteMasterFrontend.EFModels;
+using RouteMasterFrontend.Models.Infra.DapperRepositories;
+using RouteMasterFrontend.Models.Interfaces;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the DI container.
+builder.Services.AddDbContext<RouteMasterContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("RouteMaster"));
+});
+
+
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+	options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(20);
+	options.Lockout.MaxFailedAccessAttempts = 2;
+
+});
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+	options.LoginPath = new PathString("/Members/MemberLogin");
+
+	//options.LogoutPath = "Members/Logout";
+	//options.AccessDeniedPath = "/"; 存取失敗的路徑
+});
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+	options.MinimumSameSitePolicy = SameSiteMode.Lax;
+	options.HttpOnly = HttpOnlyPolicy.Always;
+	options.Secure = (CookieSecurePolicy)SameSiteMode.None; // 設置 Secure 屬性為 None
+});
+
+
+
+
+
+
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
