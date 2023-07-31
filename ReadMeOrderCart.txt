c34ea09817d30568088c72210e12111cc694ca15
kristy <s965402323@gmail.com>
2023-07-31 20:15:42 +0800

    7/31 [v] Add activitiesdetails, extraservicedetails, accomodationdetails partialview in the cart index page.

---
+++ ReadMeOrderCart.txt
@@ -0,0 +1,10 @@
+7/31
+[v] Add activitiesdetails, extraservicedetails, accomodationdetails partialview in the cart index page.
+
+
+
+Todo:
+Add product to cart
+Delete product from cart
+CheckOut page
+
--- RouteMasterFrontend/Models/EFModels/Cart.cs
+++ RouteMasterFrontend/Models/EFModels/Cart.cs
@@ -21,5 +21,6 @@ namespace RouteMasterFrontend.EFModels
         public virtual ICollection<Cart_AccommodationDetail> Cart_AccommodationDetails { get; set; }
         public virtual ICollection<Cart_ActivitiesDetail> Cart_ActivitiesDetails { get; set; }
         public virtual ICollection<Cart_ExtraServicesDetail> Cart_ExtraServicesDetails { get; set; }
+
     }
 }
