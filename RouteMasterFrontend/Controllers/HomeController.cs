165596a82488579304bd09f8fcb6ae356324e5e2
Ben Wang <fp4652@yahoo.com.tw>
2023-08-08 17:15:29 +0800

    0808 大頭貼圖案

--- RouteMasterFrontend/Controllers/HomeController.cs
+++ RouteMasterFrontend/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RouteMasterFrontend.Models;
 using System.Diagnostics;
 
 namespace RouteMasterFrontend.Controllers
 {
+	
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
