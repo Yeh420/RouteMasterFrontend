731db5e50328c18704b21a9ac529818a8b577161
Kim <vergalsaver78@gmail.com>
2023-08-09 16:20:53 +0800

    FAQIndex Fetch成功

---
+++ RouteMasterFrontend/Controllers/FAQController.cs
@@ -0,0 +1,44 @@
+﻿using MessagePack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RouteMasterFrontend.EFModels;
+using RouteMasterFrontend.Models.Dto;
+using RouteMasterFrontend.Models.ViewModels.Comments_Accommodations;
+using System.Runtime.Serialization.Json;
+
+namespace RouteMasterFrontend.Controllers
+{
+    public class FAQController : Controller
+    {
+        private readonly RouteMasterContext _context;
+        public FAQController(RouteMasterContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Search()
+        {
+            var questList = _context.FAQs
+                .Include(f => f.FAQCategory);
+
+
+            var dto = await questList.Select(f=>new FAQIndexDTO
+            {
+                Id = f.Id,
+                CategoryName=f.FAQCategory.Name,
+                Question=f.Question,
+                Answer=f.Answer,
+                Helpful=f.Helpful,
+                Image=f.Image,
+            }).ToListAsync();
+                
+                
+            return Json(dto);
+        }
+    }
+}
