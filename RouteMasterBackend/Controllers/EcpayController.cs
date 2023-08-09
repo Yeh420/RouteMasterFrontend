51ee164761a2c048c6424941af2c5a24d510a0ef
kristy <s965402323@gmail.com>
2023-08-09 21:00:50 +0800

    為什麼有雪寶??

---
+++ RouteMasterBackend/Controllers/EcpayController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace RouteMasterBackend.Controllers
+{
+    public class EcpayController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public EcpayController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpPost]
+        public IActionResult AddPayInfo(JObject info)
+        {
+            try
+            {
+                _memoryCache.Set(info.Value<string>("MerchantTradeNo"), info, DateTime.Now.AddMinutes(60));
+                return Ok("1|OK");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("0|Error");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddAccountInfo(JObject info)
+        {
+            try
+            {
+                _memoryCache.Set(info.Value<string>("MerchantTradeNo"), info, DateTime.Now.AddMinutes(60));
+                return Ok("1|OK");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("0|Error");
+            }
+        }
+    }
+}
