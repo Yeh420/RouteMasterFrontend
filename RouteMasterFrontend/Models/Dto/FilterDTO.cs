fa533b5828f4cb7f1fb09d10063312be1b3e3e6f
Yeh420 <blizzard255324@gmail.com>
2023-08-10 20:59:24 +0800

    寫篩選

--- RouteMasterFrontend/Models/Dto/FilterDTO.cs
+++ RouteMasterFrontend/Models/Dto/FilterDTO.cs
@@ -1,10 +1,11 @@
-﻿namespace RouteMasterBackend.DTOs
+﻿namespace RouteMasterFrontend.Models.Dto
 {
     public class FilterDTO
     {
         public IEnumerable<double?> Grades { get; set; }
         public IEnumerable<string> AcommodationCategories { get; set; }
+        public IEnumerable<int> CommentSorce { get; set; }
         public IEnumerable<string> ServiceInfoCategories { get; set; }
-        public IEnumerable<string> Regions{ get; set; }
+        public IEnumerable<string> Regions { get; set; }
     }
 }
