0d23b14e8fd2a541a24f40ef49c83b08aa5a1638
Kim <vergalsaver78@gmail.com>
2023-08-10 19:15:38 +0800

    住宿評論collapse

--- RouteMasterFrontend/wwwroot/js/Comments_AccommodationPartial.js
+++ RouteMasterFrontend/wwwroot/js/Comments_AccommodationPartial.js
@@ -1,4 +1,4 @@
-﻿var vueApp1 = {
+﻿const dec = {
     data() {
         return {
             indexVM: [],
@@ -6,22 +6,26 @@
             isReplyed: "已回復",
             ep: null,
             selected: 0,
-            hotelId: 1, //假設這是呈現AccomodationId=2的評論清單，這裡直接賦值=2
+            //hotelId: 1, //假設這是呈現AccomodationId=2的評論清單，這裡直接賦值=2
             thumbicon: [],
+            hotelId:0
 
         }
     },
-    created: function () {
-        let _this = this;
-    },
-    mounted: function () {
-        let _this = this;
-        _this.commentDisplay();
-    },
+    //created: function () {
+    //    let _this = this;
+    //},
+    //mounted: function () {
+    //    let _this = this;
+    //    _this.commentDisplay();
+    //},
     methods: {
-        commentDisplay: function () {
+        commentDisplay: function (id) {
             let _this = this;
             var request = {};
+            if (id) {
+                _this.hotelId = id;
+            }
             request.Manner = _this.selected;
             request.HotelId = _this.hotelId;
 
@@ -61,7 +65,83 @@
             return `../MemberUploads/${photo}`;
         }
 
-    }
+    },
+    template: `
+        <div class="container">
+        <div class="row mb-2" style="width:20%">
+            <select v-model="selected" id="commentOrder" class="ms-3" @change="commentDisplay()">
+                <option value="0" selected>排序選擇</option>
+                <option value="1">最新留言</option>
+                <option value="2">星星評分高至低</option>
+                <option value="3">星星評分低至高</option>
+            </select>
+            <div class="ms-auto"><a href="https://localhost:7145/Comments_Accommodation/Create" class="link-dark">新增評論</a></div>
+        </div>
+        <div v-for="(item, index) in indexVM" :key="index" class="card mb-3 w-75">
+            <div class="row g-0">
+                <div class="col-md-8">
+                    <div class="card-header">{{item.account}}</div>
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <h4 class="card-title me-auto">標題: {{item.title}}</h4>
+                            <div>{{item.status}}</div>
+                        </div>
+                        <p class="card-text">{{item.id}}</p>
+                        <p class="card-text">{{item.hotelName}}</p>
+                        <p class="card-text">優點: {{item.pros}}</p>
+                        <p class="card-text">缺點: {{item.cons}}</p>
+                        <p class="card-text">{{item.score}}</p>
+                        <div class="d-flex">
+                            <p class="card-text me-auto"><i class="fa-solid fa-thumbs-up fa-lg"></i> {{item.totalThumbs}}</p>
+                            <p><small class="text-muted">{{item.createDate}}</small></p>
+                        </div>
+                        
+                        <hr />
+                        <button type="button" v-html="thumbicon[index]" @click="likeComment(item.id)" class="btn btn-outline-dark me-3" data-bs-toggle="tooltip" data-bs-placement="top" title="按讚">
+                                
+                        </button>
+                       
+                        <template v-if="item.status===isReplyed">
+                            <button type="button" class="btn btn-primary position-relative" data-bs-toggle="collapse"
+                                    :data-bs-target='"#collapseExample"+index' @showReply(item.id)>
+                                看回覆訊息
+                            </button>
+                            <div class="collapse mt-3" :id='"collapseExample" + index'>
+                                <div class="card card-body">
+                                    <h5>來自{{item.hotelName}}的回覆:</h5>
+                                    <p>{{item.replyMessage}}</p>
+                                    <p class="card-text text-end"><small class="text-muted">{{item.replyDate}}</small></p>
+                                </div>
+                            </div>
+                        </template>                                             
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <template v-if="item.imageList.length>1">
+                        <div :id='"carousel" + index' class="carousel carousel-dark slide" data-bs-ride="carousel">
+                            <div class="carousel-inner w-100 mx-auto my-auto">
+                                <div :class="{ 'carousel-item': true, 'active': num === 0 }" v-for="(photo,num) in item.imageList" :key="num">
+                                    <img v-bind:src="getImgPath(photo)" class="d-block w-100">
+                                </div>
+                  
+                            </div>
+                            <button class="carousel-control-prev" type="button" :data-bs-target='"#carousel"+ index' data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" :data-bs-target='"#carousel"+ index' data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>    
+                    </template>                    
+                    <template v-else>
+                        <img src="../MemberUploads/RouteMaster.png" class="img-fluid rounded-start">
+                    </template>                   
+                </div>
+            </div>
+        </div>
+    </div>
+    `
 
 };
-const dec = Vue.createApp(vueApp1).mount("#dec");
