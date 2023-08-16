5f82397f63a4e305031416539ee10bf12a3abd11
鯊鯊 <noone183492761@gmail.com>
2023-08-17 01:26:51 +0800

    20230817 優惠券功能完成

--- RouteMasterFrontend/Controllers/CouponsController.cs
+++ RouteMasterFrontend/Controllers/CouponsController.cs
@@ -33,11 +33,11 @@ namespace RouteMasterFrontend.Controllers
             {
                 if (coupon.StartDate < now && now < coupon.EndDate)
                 {
-                    coupon.valuable = true;
+                    coupon.Valuable = true;
                 }
                 else
                 {
-                    coupon.valuable = false;
+                    coupon.Valuable = false;
                 }
             }
 
@@ -69,8 +69,8 @@ namespace RouteMasterFrontend.Controllers
             }
         }
         public bool IsActive { get; set; }
-        public bool? valuable { get; set; }
+        public bool? Valuable { get; set; }
 
-        public bool used { get; set; } = false;
+        public bool Selected { get; set; } = false;
     }
 }
