725312b746d238ee71c09334694775b3e66ef0ed
mopekim <vergalsaver78@gmail.com>
2023-08-18 00:56:06 +0800

    通知功能編輯中

---
+++ RouteMasterFrontend/Controllers/SystemMessageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RouteMasterFrontend.EFModels;
+using RouteMasterFrontend.Models.Dto;
+
+namespace RouteMasterFrontend.Controllers
+{
+    public class SystemMessageController : Controller
+    {
+        private readonly RouteMasterContext _context;
+
+        public SystemMessageController(RouteMasterContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Index()
+        {
+            var messageDb= await _context.SystemMessages
+                .Where(m=>m.MemberId ==1)
+                .Select(m=>new SystemMessageAjaxDTO
+                {
+                    Id = m.Id,
+                    Category= m.Content.Contains("檢舉") ? "檢舉" : (m.Content.Contains("按讚") ? "按讚" : "回覆"),
+                    Content = m.Content,
+                    IsRead = m.IsRead,
+                
+            
+                   
+                }).ToListAsync();
+                
+
+            return Json(messageDb);
+        }
+    }
+}
