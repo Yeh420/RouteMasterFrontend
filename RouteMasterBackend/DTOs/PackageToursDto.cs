40a9cfb653d4dc2c87ebd16f3616824a4f751d87
ian <sd180911@gmail.com>
2023-08-19 15:23:30 +0800

    小小套裝

---
+++ RouteMasterBackend/DTOs/PackageToursDto.cs
@@ -0,0 +1,27 @@
+﻿namespace RouteMasterBackend.DTOs
+{
+    public class PackageToursDto
+    {
+        public int Id { get; set; }
+        public List<actDtoInPackage>? PackageActList { get; set; }
+        public List<attDtoInPackage>? PackageAttList { get; set; }
+        public List<extDtoInPackage>? PackageExtList { get; set; }
+        public class actDtoInPackage
+        {
+            public int ActId { get; set; }
+            public string? ActName { get; set; }
+        }
+
+        public class attDtoInPackage
+        {
+            public int AttId { get; set; }
+            public string? AttName { get; set; }
+        }
+
+        public class extDtoInPackage
+        {
+            public int ExtId { get; set; }
+            public string? ExtName { get; set; }
+        }
+    }
+}
